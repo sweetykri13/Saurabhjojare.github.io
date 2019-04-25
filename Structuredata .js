29c5fade07c4c4fccb6b9ad7dfc7e006a2c3d234
Saurabh Jojare <32615499+saurabhjojare@users.noreply.github.com>
2019-04-25 19:18:56 +0530

    Structure Data

---
+++ Structuredata .js
@@ -0,0 +1,29 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Person",
+  "familyName": "Jojare",
+  "givenName": "Saurabh",
+  "url": "https://saurabhjojare.github.io",
+  "email": "saurabhjojare11@gmail.com",
+  "birthDate": "02/02/2000",
+  "birthPlace": "Beed, Maharashtra, India",
+  "affiliation": "Savitribai Phule Pune University",
+  "birthDate": "2 Feb 2000",
+  "email": "saurabhjojare11@gmail.com",
+  "alumniOf": "Savitribai Phule Pune University",
+  "gender": "http://schema.org/Male",
+  "hasOccupation": "Web Designer & Developer",
+  "nationality": "Indian",
+  "telephone": "+917083900700",
+  "image": "https://avatars1.githubusercontent.com/u/32615499?s=460&v=4",
+  
+ 
+  "sameAs": [
+    "https://www.facebook.com/jojaresaurabh",
+    "https://twitter.com/saurabhjojare",
+    "https://www.instagram.com/jojaresaurabh/",
+    "https://www.linkedin.com/in/saurabhjojare/"
+    ]
+}
+</script>
