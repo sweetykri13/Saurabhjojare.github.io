77b928094e5d264dee32b026a31c36534377243c
Saurabh Jojare <32615499+saurabhjojare@users.noreply.github.com>
2020-05-09 20:37:03 +0530

    Update and rename Structuredata .js to structuredata.js

--- structuredata.js
+++ structuredata.js
@@ -5,20 +5,19 @@
   "familyName": "Jojare",
   "givenName": "Saurabh",
   "url": "https://saurabhjojare.github.io",
-  "email": "saurabhjojare11@gmail.com",
+  "email": "saurabhjojare@gmail.com",
   "affiliation": "Savitribai Phule Pune University",
   "birthDate": "2 Feb 2000",
   "alumniOf": "Savitribai Phule Pune University",
   "gender": "http://schema.org/Male",
-  "hasOccupation": "Full-Stack Web Developer",
+  "hasOccupation": "Full-Stack Developer",
   "nationality": "Indian",
   "image": "https://avatars1.githubusercontent.com/u/32615499?s=460&v=4",
   
  
   "sameAs": [
-    "https://www.facebook.com/jojaresaurabh",
     "https://twitter.com/saurabhjojare",
-    "https://www.instagram.com/jojaresaurabh/",
+    "https://www.instagram.com/saurabh_jojare/",
     "https://www.linkedin.com/in/saurabhjojare/"
     ]
 }
