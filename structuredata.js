18cfec6edadfc753f21f173107fa34a281c243c2
Saurabh Jojare <32615499+saurabhjojare@users.noreply.github.com>
2020-05-31 22:08:40 +0530

    Update structuredata.js

--- structuredata.js
+++ structuredata.js
@@ -10,7 +10,7 @@
   "birthDate": "2 Feb 2000",
   "alumniOf": "Savitribai Phule Pune University",
   "gender": "http://schema.org/Male",
-  "hasOccupation": "Full-Stack Developer",
+  "hasOccupation": "Full Stack Developer",
   "nationality": "Indian",
   "image": "https://avatars1.githubusercontent.com/u/32615499?s=460&v=4",
   
@@ -19,6 +19,7 @@
     "https://twitter.com/saurabhjojare",
     "https://www.instagram.com/saurabh_jojare/",
     "https://www.linkedin.com/in/saurabhjojare/"
+    "https://github.com/saurabhjojare"
     ]
 }
 </script>
